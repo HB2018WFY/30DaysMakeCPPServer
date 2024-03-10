dbb1774ed8dad866ef851eb02e4cbee2a7a040e5
TopCarry <2240390154@qq.com>
2024-03-10 15:25:46 +0800

    day01

---
+++ day1/client.cpp
@@ -0,0 +1,14 @@
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<cstring>
+
+int main(){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    serv_addr.sin_port=htons(8888);
+
+    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+}
